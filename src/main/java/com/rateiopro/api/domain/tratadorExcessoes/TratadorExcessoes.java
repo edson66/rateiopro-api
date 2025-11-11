cc5900127fd575d3f29ef75ae3a6aae04c4c3651
edson66 <edsonulissesr1@gmail.com>
2025-11-11 00:18:43 -0300

    Tratamento de excessões

---
+++ src/main/java/com/rateiopro/api/domain/tratadorExcessoes/TratadorExcessoes.java
@@ -0,0 +1,43 @@
+package com.rateiopro.api.domain.tratadorExcessoes;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorExcessoes {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity excessoesRunTime(RuntimeException ex){
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity notFound(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity erroSenhaOuUsuario(){
+        return ResponseEntity.badRequest().body("Email ou senha incorreta");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity erroValidacao(MethodArgumentNotValidException ex){
+        var erros = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new));
+    }
+
+    private record DadosErroValidacao(String campo,String mensagem){
+        public DadosErroValidacao(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
+}
