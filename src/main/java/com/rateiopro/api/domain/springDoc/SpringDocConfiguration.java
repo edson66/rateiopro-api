08aad9f8e977632381d14abd9e5c518ad94b2839
Edson <edsonulissesr1@gmail.com>
2025-11-20 16:10:19 -0300

    erro swagger

--- src/main/java/com/rateiopro/api/domain/springDoc/SpringDocConfiguration.java
+++ src/main/java/com/rateiopro/api/domain/springDoc/SpringDocConfiguration.java
@@ -3,6 +3,7 @@ package com.rateiopro.api.domain.springDoc;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,7 @@ public class SpringDocConfiguration {
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
                 .info(new Info()
                         .title("RateioPro API")
                         .description("Api para divisão de despesas entre usuários")
